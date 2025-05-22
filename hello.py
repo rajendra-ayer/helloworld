744dc3010592a56cf8c6dba090258293be594a87
Rajendra Ayer <rajendra.ayer@outlook.com>
2025-05-22 19:46:44 +1000

    helloworld

---
+++ hello.py
@@ -0,0 +1,10 @@
+# app.py
+
+import streamlit as st
+
+# Title of the web app
+st.title("Hello, World!")
+
+# Main content
+st.write("Welcome to your first Streamlit app.")
+st.write("Streamlit makes it easy to build web apps with Python.")
